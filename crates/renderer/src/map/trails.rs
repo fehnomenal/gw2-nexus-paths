b007693c4827fa9969cab20efd7e4efdb5a53cc6
Andreas Fehn <fehnomenal@fehn.systems>
2024-10-13 23:43:37 +0200

    extract trail path cache into dedicated struct

--- crates/renderer/src/map/trails.rs
+++ crates/renderer/src/map/trails.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, rc::Rc};
 
 use egui::Color32;
 use log_err::LogErrResult;
@@ -6,11 +6,12 @@ use paths_core::markers::ActiveTrail;
 use paths_data::{maps::MAP_TO_WORLD_TRANSFORMATION_MATRICES, markers::simplify_line_string};
 use paths_types::settings::Settings;
 use windows::{
+    core::Interface,
     Foundation::Numerics::Matrix3x2,
     Win32::Graphics::Direct2D::{
         Common::{D2D1_COLOR_F, D2D1_FIGURE_BEGIN_HOLLOW, D2D1_FIGURE_END_OPEN, D2D_POINT_2F},
-        ID2D1SolidColorBrush, D2D1_CAP_STYLE_ROUND, D2D1_ELLIPSE, D2D1_LINE_JOIN_ROUND,
-        D2D1_STROKE_STYLE_PROPERTIES1,
+        ID2D1Factory1, ID2D1Geometry, ID2D1SolidColorBrush, D2D1_CAP_STYLE_ROUND, D2D1_ELLIPSE,
+        D2D1_LINE_JOIN_ROUND, D2D1_STROKE_STYLE_PROPERTIES1,
     },
 };
 
@@ -84,42 +85,7 @@ impl MapRenderer {
             return;
         };
 
-        let path = self.trail_path_cache.entry(trail.hash).or_insert_with(|| {
-            let path = self
-                .d2d1_factory
-                .CreatePathGeometry()
-                .log_expect("could not create path geometry");
-
-            let sink = path.Open().log_expect("could not open path geometry");
-
-            sink.BeginFigure(
-                D2D_POINT_2F {
-                    x: trail.points[0].x,
-                    y: trail.points[0].y,
-                },
-                D2D1_FIGURE_BEGIN_HOLLOW,
-            );
-
-            let points = simplify_line_string(trail.points, *settings.trail_simplify_epsilon);
-
-            sink.AddLines(
-                &points
-                    .iter()
-                    // The first point is used as the start to `BeginFigure`.
-                    .skip(1)
-                    .map(|point| D2D_POINT_2F {
-                        x: point.x,
-                        y: point.y,
-                    })
-                    .collect::<Vec<_>>(),
-            );
-
-            sink.EndFigure(D2D1_FIGURE_END_OPEN);
-
-            sink.Close().log_expect("could not close path geometry");
-
-            path
-        });
+        let path = self.trail_path_cache.get_trail_geometry(trail, settings);
 
         let stroke_style = self.trail_stroke_style.get_or_insert_with(|| {
             self.d2d1_factory
@@ -141,7 +107,7 @@ impl MapRenderer {
         let stroke_width = *trail.trail_width;
 
         self.d2d1_device_context
-            .DrawGeometry(path as &_, brush, stroke_width, stroke_style as &_);
+            .DrawGeometry(path, brush, stroke_width, stroke_style as &_);
 
         self.d2d1_device_context.FillEllipse(
             &D2D1_ELLIPSE {
@@ -154,5 +120,82 @@ impl MapRenderer {
             },
             brush,
         );
+    }
+}
+
+pub struct TrailPathCache {
+    cache: HashMap<u64, ID2D1Geometry>,
+    d2d1_factory: Rc<ID2D1Factory1>,
+}
+
+impl TrailPathCache {
+    pub fn new(d2d1_factory: Rc<ID2D1Factory1>) -> Self {
+        Self {
+            cache: HashMap::new(),
+            d2d1_factory,
+        }
+    }
+
+    unsafe fn get_trail_geometry(
+        &mut self,
+        trail: &ActiveTrail,
+        settings: &Settings,
+    ) -> &ID2D1Geometry {
+        self.cache.entry(trail.hash).or_insert_with(|| {
+            let starting_point_circle = {
+                let radius = *trail.trail_width * 2.5;
+
+                D2D1_ELLIPSE {
+                    point: D2D_POINT_2F {
+                        x: trail.points[0].x,
+                        y: trail.points[0].y,
+                    },
+                    radiusX: radius,
+                    radiusY: radius,
+                }
+            };
+
+            Self::build_path_geometry(
+                &self.d2d1_factory,
+                &starting_point_circle.point,
+                trail,
+                settings,
+            )
+        })
+    }
+
+    unsafe fn build_path_geometry(
+        d2d1_factory: &ID2D1Factory1,
+        starting_point: &D2D_POINT_2F,
+        trail: &ActiveTrail,
+        settings: &Settings,
+    ) -> ID2D1Geometry {
+        let path = d2d1_factory
+            .CreatePathGeometry()
+            .log_expect("could not create path geometry");
+
+        let sink = path.Open().log_expect("could not open path geometry");
+
+        sink.BeginFigure(*starting_point, D2D1_FIGURE_BEGIN_HOLLOW);
+
+        let points = simplify_line_string(trail.points, *settings.trail_simplify_epsilon);
+
+        sink.AddLines(
+            &points
+                .iter()
+                // The first point is used as the start to `BeginFigure`.
+                .skip(1)
+                .map(|point| D2D_POINT_2F {
+                    x: point.x,
+                    y: point.y,
+                })
+                .collect::<Vec<_>>(),
+        );
+
+        sink.EndFigure(D2D1_FIGURE_END_OPEN);
+
+        sink.Close().log_expect("could not close path geometry");
+
+        path.cast().log_unwrap()
     }
 }
